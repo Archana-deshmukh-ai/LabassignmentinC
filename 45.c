36d7b41bca9b0ffc55e1c0c1db6f7dbf95933623
Archana-deshmukh-ai <u24ai077@aid.svnit.ac.in>
2024-11-27 10:01:13 +0530

    Add files via upload

---
+++ 45.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+   int n;
+    printf("Enter your number: ");
+    scanf("%d",&n);
+    (n==7 ) ? (printf("yeah!This is the lucky number")):(printf("Better luck next time "));
+    return 0;
+}
